6951e57f78ceefb16aa722bc4367360289fef591
tarun <ahirwar.tarun@thinksys.com>
2024-10-08 18:36:29 +0530

    added error handling by creating a custom error file where I created a customError instance class and used it to generate dynamic error by passing the msg and error code in the function

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -6,6 +6,7 @@ const connectDB = require('./db/connect');
 require('dotenv').config();
 
 const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware to run static file like html
 app.use(express.static('./public'));
@@ -16,14 +17,13 @@ app.use(express.json());
 app.use('/api/v1/tasks', tasks);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(
-      process.env.PORT,
-      console.log(`server is listening on port ${process.env.PORT}....`)
-    );
+    app.listen(port, console.log(`server is listening on port ${port}....`));
   } catch (error) {
     console.log(error);
   }
